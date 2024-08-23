1999cb802d573712fb4a59d0e627f6971daf2fc2
Matvei98 <Matthias78@yandex.ru>
2024-08-23 17:08:42 +0300

    Import api to apiNews.js

--- my-news/src/pages/Main/Main.jsx
+++ my-news/src/pages/Main/Main.jsx
@@ -1,18 +1,18 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState} from "react";
 import { NewsBanner } from "NewsBanner/NewsBanner";
 import styles from "./styles.module.css";
 import { getNews } from "api/apiNews";
-import { NewsList } from 'NewsList/NewsList';
+// import { NewsList } from 'NewsList/NewsList';
 
 
 export const Main = () => {
-  const [news, setNews] = useState([]);
+  const [news,setNews] = useState([]);
 
   useEffect(() => {
     const fetchNews = async () => {
       try {
-        const response = await getNews();
-        setNews(response.news);
+      const response = await getNews();
+       setNews(response.news);
       } catch (error) {
         console.log(error);
       }
@@ -22,8 +22,7 @@ export const Main = () => {
 
   return (
     <main className={styles.main}>
-    {news.length > 0 ? <NewsBanner item={news[0]}/> : null}
-      <NewsList news={news} />
+   {news.length > 0 ? <NewsBanner item={news[0]}/>:null}
     </main>
   );
 };
